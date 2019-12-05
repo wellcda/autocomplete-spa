4d9df0ce34d2c4f0173df6e327583ce2248959f2
Wellington Aguiar <wellington@castrodeaguiar.com>
2019-12-05 14:34:33 -0300

    master | creating component and service files for the future country search component

---
+++ src/app/components/country-search/country-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-search',
+  templateUrl: './country-search.component.html',
+  styleUrls: ['./country-search.component.scss']
+})
+export class CountrySearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
