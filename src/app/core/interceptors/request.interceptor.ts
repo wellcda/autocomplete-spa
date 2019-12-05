b9920f9bd4c7c7713b03d94954d601d5a14db595
Wellington Aguiar <wellington@castrodeaguiar.com>
2019-12-05 16:11:49 -0300

    feature/request-interceptor | creating core folder to hold interceptors and services, creating first draft of request interceptor

---
+++ src/app/core/interceptors/request.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, filter } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestInterceptor implements HttpInterceptor {
+  constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const url = `localhost:8000/api${request.url}`;
+
+    request = request.clone({
+      url,
+    });
+
+    return next.handle(request).pipe(
+      filter((response: HttpEvent<any>) => response instanceof HttpResponse),
+      map((response: any) => {
+        response.body = response.body.data;
+        return response;
+      }),
+      catchError(error => throwError(error))
+    );
+  }
+}
